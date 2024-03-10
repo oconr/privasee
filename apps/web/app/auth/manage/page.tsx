ccfb63d47d8f89b44b489b2d758b0cf53dbdd470
Ryan O'Connor <github@oconr.uk>
2024-03-10 18:06:53 +0000

    Refactor manage page component

--- apps/web/app/auth/manage/page.tsx
+++ apps/web/app/auth/manage/page.tsx
@@ -1,8 +1,6 @@
 import { cookies } from "next/headers";
 import { DataTable } from "../data-table";
-import { manageColumns, Question } from "../columns";
-import { Button } from "ui/components/ui/button";
-import Link from "next/link";
+import { manageColumns } from "../columns";
 
 export default async function Dashboard() {
   const session = cookies().get("session_jwt").value;
@@ -18,12 +16,12 @@ export default async function Dashboard() {
 
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-row items-center justify-end w-full mb-2">
-        <Button asChild variant="secondary">
-          <Link href="/manage/new">Add question</Link>
-        </Button>
-      </div>
-      <DataTable columns={manageColumns} data={questions} />
+      <DataTable
+        columns={manageColumns}
+        data={questions}
+        bulkAssign
+        bulkDelete
+      />
     </div>
   );
 }
